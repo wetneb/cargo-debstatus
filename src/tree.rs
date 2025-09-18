2c09e468044baee94199f97aa1cd8c1a8cc92f61
Antonin Delpeuch <antonin@delpeuch.eu>
2025-09-18 19:43:00 +0200

    Display `(*)` next to the already visited crates

--- src/tree.rs
+++ src/tree.rs
@@ -200,6 +200,7 @@ fn print_package<'a, W: Write>(
     levels_continue: &mut Vec<bool>,
     writer: &mut W,
 ) -> Result<(), Error> {
+    let already_visited = !visited_deps.insert(&package.id);
     let treeline = {
         let mut line = format!(" {} ", package.packaging_status());
         match prefix {
@@ -232,12 +233,11 @@ fn print_package<'a, W: Write>(
         )?;
     } else {
         let pkg_status_s = format::human::display(format, package)?;
-        writeln!(writer, "{treeline}{pkg_status_s}")?;
+        let already_visited_s = if already_visited { " (*)" } else { "" };
+        writeln!(writer, "{treeline}{pkg_status_s}{already_visited_s}")?;
     }
 
-    if !all && !package.show_dependencies() && !levels_continue.is_empty()
-        || !visited_deps.insert(&package.id)
-    {
+    if !all && !package.show_dependencies() && !levels_continue.is_empty() || already_visited {
         return Ok(());
     }
 
@@ -397,7 +397,7 @@ mod tests {
         let expected = r#" 🪏  cargotest v0.1.0 (in workspace, /tmp/cargotest)
  🔴 └── crossbeam-channel v0.5.15
  🔴     └── crossbeam-utils v0.8.21
- 🔴         └── crossbeam-channel v0.5.15
+ 🔴         └── crossbeam-channel v0.5.15 (*)
 "#;
         assert_eq!(String::from_utf8(buffer)?, expected);
         Ok(())
